a2d4a7b3c9103d6c02044621fc7275ad5722a9fd
Tuan Nguyen <ngoctuanit07@gmail.com>
2023-10-06 16:59:29 +0700

    update code

--- tenmien.py
+++ tenmien.py
@@ -2,50 +2,50 @@ import requests
 from bs4 import BeautifulSoup
 from googlesearch import search
 import time
-import sqlite3
 import datetime
 import matplotlib.pyplot as plt
 from flask import Flask, render_template
+import mysql.connector
 
 # User-Agent giả mạo để giúp tránh chặn
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'
 }
 
+# Kết nối đến cơ sở dữ liệu MySQL
+conn = mysql.connector.connect(
+    host="your_mysql_host",
+    user="your_mysql_user",
+    password="your_mysql_password",
+    database="your_mysql_database"
+)
+cursor = conn.cursor()
+
 # Tạo ứng dụng Flask
 app = Flask(__name__)
 
 def create_database():
-    # Tạo cơ sở dữ liệu SQLite
-    conn = sqlite3.connect('keyword_rankings.db')
-    cursor = conn.cursor()
-
     # Tạo bảng để lưu trữ dữ liệu về thứ hạng từ khóa
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS keyword_rankings (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        keyword TEXT,
-        domain TEXT,
-        position INTEGER,
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        keyword VARCHAR(255),
+        domain VARCHAR(255),
+        position INT,
         date DATE
     )
     ''')
 
-    # Lưu thay đổi và đóng kết nối
+    # Lưu thay đổi
     conn.commit()
-    conn.close()
 
 def update_database(domain, keywords_to_check, max_results=10):
-    # Kết nối đến cơ sở dữ liệu SQLite
-    conn = sqlite3.connect('keyword_rankings.db')
-    cursor = conn.cursor()
-
     # Trong vòng lặp khi kiểm tra từ khóa
     for keyword in keywords_to_check:
         keyword_found = False
 
         # Lặp qua các kết quả tìm kiếm
-        for i, result in enumerate(search(f"{domain} {keyword}", max_results)):
+        for i, result in enumerate(search(f"{domain} {keyword}", num=max_results, stop=max_results, pause=2)):
             try:
                 response = requests.get(result, headers=headers)
                 soup = BeautifulSoup(response.text, 'html.parser')
@@ -62,24 +62,17 @@ def update_database(domain, keywords_to_check, max_results=10):
         if keyword_found:
             # Lưu thứ hạng từ khóa và ngày tương ứng vào cơ sở dữ liệu
             date = datetime.date.today()
-            cursor.execute("INSERT INTO keyword_rankings (keyword, domain, position, date) VALUES (?, ?, ?, ?)", (keyword, domain, position, date))
+            cursor.execute("INSERT INTO keyword_rankings (keyword, domain, position, date) VALUES (%s, %s, %s, %s)", (keyword, domain, position, date))
             conn.commit()
         else:
             # Nếu không tìm thấy, lưu 'Không tìm thấy'
             date = datetime.date.today()
-            cursor.execute("INSERT INTO keyword_rankings (keyword, domain, position, date) VALUES (?, ?, ?, ?)", (keyword, domain, "Không tìm thấy", date))
+            cursor.execute("INSERT INTO keyword_rankings (keyword, domain, position, date) VALUES (%s, %s, %s, %s)", (keyword, domain, "Không tìm thấy", date))
             conn.commit()
 
-    # Đóng kết nối
-    conn.close()
-
 def plot_keyword_rankings(keyword):
-    # Kết nối đến cơ sở dữ liệu SQLite
-    conn = sqlite3.connect('keyword_rankings.db')
-    cursor = conn.cursor()
-
     # Truy vấn dữ liệu từ cơ sở dữ liệu cho từ khóa cụ thể
-    cursor.execute("SELECT date, position FROM keyword_rankings WHERE keyword=?", (keyword,))
+    cursor.execute("SELECT date, position FROM keyword_rankings WHERE keyword=%s", (keyword,))
     data = cursor.fetchall()
 
     if not data:
@@ -101,23 +94,27 @@ def plot_keyword_rankings(keyword):
     # Hiển thị biểu đồ
     plt.show()
 
-    # Đóng kết nối
-    conn.close()
-
 @app.route('/')
 def index():
-    domain = "taphoahaanh.net"  # Thay thế bằng tên miền bạn muốn theo dõi
-    keywords_to_check = ["Áo thun SOLEIL ROOM", "Tạp hóa hà anh"]  # Thay thế bằng danh sách từ khóa của bạn
+    # Đọc cặp tên miền và danh sách từ khóa từ tệp tin config.txt
+    with open('config.txt', 'r') as file:
+        lines = file.read().splitlines()
+    pairs = [line.split(',') for line in lines]
 
     # Bước 1: Tạo cơ sở dữ liệu nếu chưa tồn tại
     create_database()
 
-    # Bước 2: Cập nhật và lưu dữ liệu vào cơ sở dữ liệu
-    update_database(domain, keywords_to_check)
+    # Bước 2: Duyệt qua từng cặp tên miền và danh sách từ khóa
+    for pair in pairs:
+        domain = pair[0]
+        keywords_to_check = pair[1:]
+        
+        # Cập nhật và lưu dữ liệu vào cơ sở dữ liệu cho tên miền và danh sách từ khóa hiện tại
+        update_database(domain, keywords_to_check)
 
-    # Bước 3: Vẽ biểu đồ thứ hạng từ khóa cho từng từ khóa
-    for keyword in keywords_to_check:
-        plot_keyword_rankings(keyword)
+        # Vẽ biểu đồ thứ hạng từ khóa cho từng từ khóa của tên miền hiện tại
+        for keyword in keywords_to_check:
+            plot_keyword_rankings(keyword)
 
 if __name__ == "__main__":
     app.run(debug=True)
